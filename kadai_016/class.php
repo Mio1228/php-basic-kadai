f0a99a85e7c4ec3ee162f1754af3cd1e405a703b
Mio1228 <waterway1228@gmail.com>
2025-10-30 06:55:58 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,38 @@
+
+<?php
+    class Food {
+        public $name; 
+        public $price; 
+
+        public function show_price($price) {
+        echo $this -> price;
+        }
+
+        public function __construct($name, $price){
+            $this -> name = $name;
+            $this -> price =$price;
+        }
+    }
+
+    class Animal{
+        public $name;
+        public $height;
+        public $weight;
+
+        public function show_height($height){
+            echo $this -> height;
+        } 
+
+        public function __construct($name, $height, $weight){
+            $this -> name = $name;
+            $this -> height= $height;
+            $this -> weight =$weight;
+        }
+    }
+
+    $onion = new Food('玉ねぎ', 30);
+    print_r($onion);
+    echo '<br>';
+    $bird = new Animal('フクロウ', 100, 60);
+    print_r($bird);
+?>
