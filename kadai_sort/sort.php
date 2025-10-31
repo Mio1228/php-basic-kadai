62ec985226e292847804305b7d159ec2fbfab2f1
Mio1228 <waterway1228@gmail.com>
2025-10-31 19:45:21 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -11,21 +11,19 @@
         <?php
 
             function sort_2way($array, $order){
-                $nums =[15, 4, 18, 23, 10];
-             
                 
                 if ($order){
                     echo '昇順にソートします'. '<br>';
-                   sort($nums);
-                   foreach ($nums as $num){
+                   sort($array);
+                   foreach ($array as $num){
                     echo $num.'<br>';
                    }
                    echo '<br>';
                 }
                 else{
                     echo '降順にソートします' . '<br>';
-                    rsort($nums);
-                    foreach ($nums as $num){
+                    rsort($array);
+                    foreach ($array as $num){
                         echo $num.'<br>';
                     };
                 }
